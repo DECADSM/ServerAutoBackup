bb60bb8bfc14f4ea9d9a1873bbbad7e94fff3250
Ethan Villasin <duke129409@gmail.com>
2026-05-29 18:12:33 -0400

    Basic zipping functions implemented, Files for Minecraft setup

--- AutoSaveLogic.py
+++ AutoSaveLogic.py
@@ -1,6 +1,74 @@
-selectedGame = ""
+import os, datetime, zipfile
 
+selectedGame = ""
+serverPath = ""
+backupPath = ""
+logBox = ""
+
+def setLogBox(textbox):
+    logBox = textbox
+    
 def SetGame(choice):
     global selectedGame
     selectedGame = choice
     print("Game set: ", selectedGame)
+    
+def SetServerPath(path, writeFN, backup):
+    global serverPath
+    global backupPath
+    if path:
+        if not os.path.exists(path):
+            writeFN("This is not a server path\n")
+            print("This is not a server path")
+            return
+        serverPath = path
+        writeFN(f"Server Path set: {serverPath}\n")
+        print("Server Path set: ", serverPath)
+    else:
+        writeFN("No server path detected\n")
+        print("No server path detected")
+        
+    if backup:
+        backupPath = backup
+        writeFN("Backup path given\n")
+    else:
+        backupPath = ""
+    
+    
+def BackUpDir():
+    global backupPath
+    if not backupPath:
+        backupPath = os.path.join(serverPath, "Backups")
+        os.makedirs(backupPath)
+        
+def GetImportantFiles(writeFN):
+    BackUpDir()
+    files = []
+    match selectedGame:
+        case "Minecraft":
+            files.append(os.path.join(serverPath, "world"))
+            files.append(os.path.join(serverPath, "server.properties"))
+            files.append(os.path.join(serverPath, "ops.json"))
+            files.append(os.path.join(serverPath, "usercache.json"))
+            files.append(os.path.join(serverPath, "user_jvm_args.txt"))
+            files.append(os.path.join(serverPath, "config"))
+            files.append(os.path.join(serverPath, "mods"))
+            files.append(os.path.join(serverPath, "defaultconfigs"))
+            files.append(os.path.join(serverPath, "tacz"))
+            files.append(os.path.join(serverPath, "tacz_backup"))
+            files.append(os.path.join(serverPath, "patchouli_books"))
+            files.append(os.path.join(serverPath, "moonlight-global-datapacks"))
+            files.append(os.path.join(serverPath, "journeymap"))
+            files.append(os.path.join(serverPath, "thingpacks"))
+        #case "Vintage Story":
+            
+        #case "Arma 3":
+    for i in files:
+        if not os.path.exists(i):
+            writeFN(f"{i} does not exsist")
+    #writeFN("paths exsist")
+    
+    time = datetime.datetime.now().strftime("%m-%d-%Y_%Hhr-%Mm")
+    with zipfile.ZipFile(os.path.join(backupPath, "Backup_" + time + ".zip"), "w") as backup:
+        for file in files:
+            backup.write(file, arcname=os.path.basename(file))
--- ServerAutoSave.py
+++ ServerAutoSave.py
@@ -6,7 +6,8 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 app = customtkinter.CTk()
-app.geometry("1280x720")
+app.geometry("600x500")
+app.resizable(False, False)
 app.title("Server Auto Save")
 
 
@@ -14,33 +15,46 @@ app.title("Server Auto Save")
 #   print("Button Clicked, Fun times ahead")
     
 def backup_button():
+    AutoSaveLogic.SetServerPath(directorypath.get(), WriteToTextBox, backupPath.get())
+    AutoSaveLogic.GetImportantFiles(WriteToTextBox)
     print("Making Backup")
     
 def game_dropdown(choice):
     print("Game chosen: ", choice)
     AutoSaveLogic.SetGame(choice)
     
-def WriteToTextBox(textbox, text):
-    textbox.configure(state="enabled")
+def WriteToTextBox(text):
+    textbox.configure(state="normal")
     textbox.insert("end", text)
     textbox.configure(state="disabled")
-    
-button = customtkinter.CTkButton(app, text="Back up server", command=backup_button)
-#button.grid(row=0,column=0)
-button.pack(pady=10, padx=10) #positon in the window
 
-gamedropdown = customtkinter.CTkOptionMenu(app, values=["Vintage Story", "Minecraft", "Arma 3"], command=game_dropdown)
-gamedropdown.pack(pady=20, padx=20)
-gamedropdown.set("Games") #Set is a method to label the widget
+#Order of the .pack() call decides what goes on top
 
-#Used to display when back up is finished or in progress and all back up zip files
-textbox = customtkinter.CTkTextbox(app)
-textbox.pack(pady=30, padx=30)
-textbox.insert("0.0", "Log:\n")
-textbox.configure(state="disabled")
+pathFrame = customtkinter.CTkFrame(master=app, width=600, height=200, fg_color="transparent")
+pathFrame.pack(pady=10, padx=10)
 
 #Input for Server Directory
-directorypath = customtkinter.CTkEntry(app, placeholder_text="Path to Server")
-directorypath.pack(pady=40, padx=40)
+directorypath = customtkinter.CTkEntry(master=pathFrame, placeholder_text="Path to Server", width=500)
+directorypath.pack(pady=20, padx=10)
+
+backupPath = customtkinter.CTkEntry(master=pathFrame, placeholder_text="Path to Backup", width=500)
+backupPath.pack()
+
+frame = customtkinter.CTkFrame(master=app, width=600, height=200, fg_color="transparent")
+frame.pack(pady=10, padx=10)
+
+gamedropdown = customtkinter.CTkOptionMenu(master=frame, values=["Vintage Story", "Minecraft", "Arma 3"], command=game_dropdown)
+gamedropdown.pack(pady=10, padx=10, side="left")
+gamedropdown.set("Games") #Set is a method to label the widget
+
+button = customtkinter.CTkButton(master=frame, text="Back up server", command=backup_button)
+#button.grid(row=0,column=0)
+button.pack(pady=20, padx=10, side="right") #positon in the window
+
+#Used to display when back up is finished or in progress and all back up zip files
+textbox = customtkinter.CTkTextbox(app, width=500)
+textbox.pack(pady=10, padx=10)
+textbox.insert("0.0", "Log:\n")
+textbox.configure(state="disabled")
 
 app.mainloop()
